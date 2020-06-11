3931ce21b1d9d1fb26e8c957335beb38647cba37
Gianmarco Roggiolani <33184864+theroggio@users.noreply.github.com>
2020-06-11 04:53:30 +0200

    ora puoi creare nuovi messaggi, ma tocca capire come fare il 'cerca' tra gli utenti sennò gli arriva col cazzo

--- app/src/main/java/com/example/huc_project/chat/NewMessage.java
+++ app/src/main/java/com/example/huc_project/chat/NewMessage.java
@@ -1,8 +1,154 @@
 package com.example.huc_project.chat;
 
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.example.huc_project.R;
+import com.example.huc_project.homepage.CreateNewPostActivity;
+import com.example.huc_project.profile.Profile_main_page;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewMessage extends AppCompatActivity {
 
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_message);
+        setUpCircularMenu();
+    }
+
+    public void send_text(View v){
+        String who = ((EditText)findViewById(R.id.user_to_contact)).getText().toString();
+        String what = ((EditText)findViewById(R.id.new_text)).getText().toString();
+        HashMap<String,String> to_put = new HashMap<>();
+        to_put.put("User1", mAuth.getCurrentUser().getUid());
+        to_put.put("User2", who );
+        to_put.put("Messages", what);
+        db.collection("Chats").document(UUID.randomUUID().toString()).set(to_put);
+        Intent i = new Intent(getBaseContext(), Chat.class);
+        startActivity(i);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.clear();
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        MenuItem addItem = menu.findItem(R.id.add_icon);
+        return true;
+    }
+
+
+    private void setUpCircularMenu(){
+        final ImageView icon = new ImageView(this);
+        final Drawable menu_ic_id = getResources().getDrawable(R.drawable.ic_menu);
+        final Drawable add_ic_id = getResources().getDrawable(R.drawable.ic_add);
+        icon.setImageDrawable(menu_ic_id);
+
+        FloatingActionButton actionButton = new FloatingActionButton.Builder(this).setContentView(icon).build();
+
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+        FloatingActionButton.LayoutParams params=new FloatingActionButton.LayoutParams(220,220);
+        itemBuilder.setLayoutParams(params);
+
+        //settings
+        ImageView settingsItem = new ImageView(this);
+        settingsItem.setImageDrawable(getResources().getDrawable(R.drawable.ic_settings));
+        SubActionButton settingsButton = itemBuilder.setContentView(settingsItem).build();
+        //chat
+        ImageView chatItem = new ImageView(this);
+        chatItem.setImageDrawable(getResources().getDrawable(R.drawable.ic_chat));
+        SubActionButton chatButton = itemBuilder.setContentView(chatItem).build();
+        //profile
+        ImageView profItem = new ImageView(this);
+        profItem.setImageDrawable(getResources().getDrawable(R.drawable.ic_profile));
+        SubActionButton profButton = itemBuilder.setContentView(profItem).build();
+        //new post
+        ImageView addItem = new ImageView(this);
+        addItem.setImageDrawable(add_ic_id);
+        SubActionButton addButton = itemBuilder.setContentView(addItem).build();
+
+        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(settingsButton)
+                .addSubActionView(chatButton)
+                .addSubActionView(profButton)
+                .addSubActionView(addButton)
+                .setRadius(470)
+                .attachTo(actionButton)
+                .build();
+
+        actionMenu.setStateChangeListener(new FloatingActionMenu.MenuStateChangeListener() {
+            @Override
+            public void onMenuOpened(FloatingActionMenu menu) {
+                icon.setImageDrawable(add_ic_id);
+                icon.setRotation(45);
+            }
+
+            @Override
+            public void onMenuClosed(FloatingActionMenu menu) {
+                icon.setImageDrawable(menu_ic_id);
+                icon.setRotation(0);
+            }
+        });
+
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CreateNewPostActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        profButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Profile_main_page.class);
+                startActivity(intent);
+            }
+        });
+
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Chat.class);
+                startActivity(i);
+            }
+        });
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
 
 }
