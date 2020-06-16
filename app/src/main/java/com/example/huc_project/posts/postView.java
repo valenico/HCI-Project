995e39c831166685e25aadc8f879a1f51e645c79
TommasoCarlini <to.carlini@gmail.com>
2020-06-16 12:23:02 +0200

    manage favorite post (avoid repetition)

--- app/src/main/java/com/example/huc_project/posts/postView.java
+++ app/src/main/java/com/example/huc_project/posts/postView.java
@@ -11,9 +11,14 @@ import com.example.huc_project.homepage.DataGettingActivity;
 import com.example.huc_project.homepage.Homepage;
 import com.example.huc_project.homepage.Post;
 import com.example.huc_project.homepage.PostRow;
+import com.example.huc_project.profile.Favorite;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.appcompat.widget.Toolbar;
@@ -27,10 +32,18 @@ import android.widget.Toast;
 
 import com.example.huc_project.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.protobuf.StringValue;
+import com.google.protobuf.StringValueOrBuilder;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class postView extends AppCompatActivity {
@@ -56,22 +69,84 @@ public class postView extends AppCompatActivity {
         this.title_view = findViewById(R.id.postTitle);
         this.desc_view = findViewById(R.id.postDesc);
 
+        final ArrayList<String> fav_post = new ArrayList<>();
+        db.collection("UTENTI").document(mAuth.getCurrentUser().getUid())
+                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.get("Fav_post") != null) {
+                        ArrayList<String> l = (ArrayList<String>) document.get("Fav_post");
+                        for (int i=0; i<l.size(); i++) { fav_post.add(l.get(i)); }
+                    }
+                } else {
+                    Log.w("lola", "Error getting documents.", task.getException());
+                }
+            }
+        });
 
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Drawable like = AppCompatResources.getDrawable(getApplicationContext(), R.drawable.ic_baseline_favorite_24);
+        final Drawable wrappedDrawable = DrawableCompat.wrap(like);
+        for (int i=0; i<fav_post.size(); i++) {
+            if (intent.getStringExtra("storageref").equals(fav_post.get(i))) {
+                DrawableCompat.setTint(wrappedDrawable, Color.RED); // TODO cercare di farlo diventare rosso se già esistente nei preferiti
+                fab.setImageDrawable(wrappedDrawable);
+                break;
+            }
+        }
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                HashMap<String,Object> data = new HashMap<>();
-                data.put("title",intent.getStringExtra("title"));
-                data.put("user", intent.getStringExtra("user"));
-                data.put("postdesc",intent.getStringExtra("desc") );
-                data.put("isPackage",intent.getBooleanExtra("isPackage", false));
-                data.put("storageref",intent.getStringExtra("storageref"));
-                db.collection("Favorites").document(mAuth.getUid()).collection("post").add(data);
-                Toast.makeText(postView.this, intent.getStringExtra("title").toString() + " added to your favorites posts!", Toast.LENGTH_LONG).show();
-                Drawable like = AppCompatResources.getDrawable(getApplicationContext(), R.drawable.ic_baseline_favorite_24);
-                Drawable wrappedDrawable = DrawableCompat.wrap(like);
-                DrawableCompat.setTint(wrappedDrawable, Color.RED);
+
+                Boolean add_post = true;
+                for (int i=0; i<fav_post.size(); i++) {
+                    if (intent.getStringExtra("storageref").equals(fav_post.get(i))) {
+                        add_post = false;
+                        break;
+                    }
+                    else {
+                        add_post = true;
+                    }
+                }
+                if (add_post) {
+                    HashMap<String,Object> data = new HashMap<>();
+                    data.put("title",intent.getStringExtra("title"));
+                    data.put("user", intent.getStringExtra("user"));
+                    data.put("postdesc",intent.getStringExtra("desc") );
+                    data.put("isPackage",intent.getBooleanExtra("isPackage", false));
+                    data.put("storageref",intent.getStringExtra("storageref"));
+                    db.collection("Favorites").document(mAuth.getUid()).collection("post").add(data);
+                    fav_post.add(intent.getStringExtra("storageref"));
+                    HashMap<String, ArrayList<String>> post_favorites = new HashMap<>();
+                    post_favorites.put("Fav_post", fav_post);
+                    db.collection("UTENTI").document(mAuth.getCurrentUser().getUid()).set(post_favorites, SetOptions.merge());
+                    Toast.makeText(postView.this, intent.getStringExtra("title").toString() + " added to your favorites posts!", Toast.LENGTH_LONG).show();
+                    DrawableCompat.setTint(wrappedDrawable, Color.RED);
+                }
+                else {
+                    fav_post.remove(intent.getStringExtra("storageref"));
+                    HashMap<String, ArrayList<String>> post_favorites = new HashMap<>();
+                    post_favorites.put("Fav_post", fav_post);
+                    db.collection("UTENTI").document(mAuth.getCurrentUser().getUid()).set(post_favorites, SetOptions.merge());
+                    DrawableCompat.setTint(wrappedDrawable, Color.GREEN); //TODO Cercare di rimettere il colore originale
+                    Toast.makeText(postView.this, intent.getStringExtra("title").toString() + " removed from your favorites posts!", Toast.LENGTH_LONG).show();
+                    db.collection("Favorites").document(mAuth.getCurrentUser().getUid()).collection("post")
+                            .whereEqualTo("storageref", intent.getStringExtra("storageref")).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    document.getReference().delete();
+                                }
+                            } else {
+                                Log.w("lola", "Error getting documents.", task.getException());
+                            }
+                        }
+                    });
+
+                }
                 fab.setImageDrawable(wrappedDrawable);
             }
         });
