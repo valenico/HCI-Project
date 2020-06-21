1c8aab4e9123c885abbecb54a62bc930ba8bd17c
TommasoCarlini <to.carlini@gmail.com>
2020-06-21 17:11:33 +0200

    fixed social icon problems and modified dele account procedure

--- app/src/main/java/com/example/huc_project/profile/RecyclerViewAdapterSocial2.java
+++ app/src/main/java/com/example/huc_project/profile/RecyclerViewAdapterSocial2.java
@@ -1,8 +1,10 @@
 package com.example.huc_project.profile;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.ClipData;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.util.Log;
@@ -126,46 +128,94 @@ public class RecyclerViewAdapterSocial2 extends RecyclerView.Adapter<RecyclerVie
         final String current_user = Profile_main_page.getCurrent_user();
         //final FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
 
-        SocialRow item = itemsList.get(position);
+        final SocialRow item = itemsList.get(position);
         String name = item.social.name;
         final String followers = item.social.followers;
         final String identity = item.social.identity;
         TextView tv1 = (TextView) viewHolder.tvItem.getChildAt(1);
         TextView tv2 = (TextView) viewHolder.tvItem.getChildAt(2);
-        ImageView view = (ImageView) viewHolder.tvItem.getChildAt(0);
+        final ImageView view = (ImageView) viewHolder.tvItem.getChildAt(0);
         tv1.setText(name);
         tv2.setText(followers + " followers");
-        Profile_prof_frag.glideTask(item.glide, Integer.parseInt(identity), view);
-
-        final CheckBox checkBox = (CheckBox) viewHolder.tvItem.getChildAt(4);
+        switch (name) {
+            case "instagram":
+                Profile_prof_frag.glideTask(item.glide, R.drawable.ic_instagram, view);
+                break;
+            case "facebook":
+                Profile_prof_frag.glideTask(item.glide, R.drawable.ic_facebook, view);
+                break;
+            case "spotify":
+                Profile_prof_frag.glideTask(item.glide, R.drawable.ic_spotify, view);
+                break;
+            case "snapchat":
+                Profile_prof_frag.glideTask(item.glide, R.drawable.ic_snapchat, view);
+                break;
+            case "github":
+                Profile_prof_frag.glideTask(item.glide, R.drawable.ic_github, view);
+                break;
+            case "linkedin":
+                Profile_prof_frag.glideTask(item.glide, R.drawable.ic_linkedin, view);
+                break;
+            case "tumblr":
+                Profile_prof_frag.glideTask(item.glide, R.drawable.ic_tumblr, view);
+                break;
+            case "twitch":
+                Profile_prof_frag.glideTask(item.glide, R.drawable.ic_twitch, view);
+                break;
+            case "twitter":
+                Profile_prof_frag.glideTask(item.glide, R.drawable.ic_twitter, view);
+                break;
+            case "vimeo":
+                Profile_prof_frag.glideTask(item.glide, R.drawable.ic_vimeo, view);
+                break;
+            case "youtube":
+                Profile_prof_frag.glideTask(item.glide, R.drawable.ic_youtube, view);
+                break;
+            case "reddit":
+                Profile_prof_frag.glideTask(item.glide, R.drawable.ic_reddit, view);
+                break;
+            default:
+                Profile_prof_frag.glideTask(item.glide, R.drawable.ic_vk, view);
+                break;
+        }
         ImageButton delete_account = (ImageButton) viewHolder.tvItem.getChildAt(3);
         delete_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (checkBox.isChecked()) {
-                    CollectionReference collection = db.collection("Social");
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("Delete account")
+                        .setMessage("Are you sure you want to delete this " + item.social.name + " account?")
 
-                    collection.whereEqualTo("user", current_user)
-                            .whereEqualTo("followers", followers)
-                            .whereEqualTo("identity", identity).get()
-                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                                       @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-                                                       @Override
-                                                       public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                                           if (task.isSuccessful()) {
-                                                               for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
-                                                                   db.collection("Social").document(document.getId()).delete();
-                                                                   Intent intent = new Intent(mContext, RemoveSocialAccount.class);
-                                                                   intent.addFlags(intent.FLAG_ACTIVITY_NEW_TASK);
-                                                                   mContext.startActivity(intent);
-                                                               }
-                                                           }
-                                                       }
-                                                   });
-                }
+                        // Specifying a listener allows you to take an action before dismissing the dialog.
+                        // The dialog is automatically dismissed when a dialog button is clicked.
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                CollectionReference collection = db.collection("Social");
+
+                                collection.whereEqualTo("user", current_user)
+                                        .whereEqualTo("followers", followers)
+                                        .whereEqualTo("identity", identity).get()
+                                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                                            @Override
+                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                if (task.isSuccessful()) {
+                                                    for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                                        db.collection("Social").document(document.getId()).delete();
+                                                        Intent intent = new Intent(mContext, RemoveSocialAccount.class);
+                                                        intent.addFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+                                                        mContext.startActivity(intent);
+                                                    }
+                                                }
+                                            }
+                                        });                            }
+                        })
+
+                        // A null listener allows the button to dismiss the dialog and take no further action.
+                        .setNegativeButton(android.R.string.no, null)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
             }
         });
     }
-
-
 }
