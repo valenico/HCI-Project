b05d01b80b953a356a7bbd368892e5b99cd5eb8b
valenico <valerio.nicolanti@hotmail.it>
2020-07-12 17:37:41 +0200

    fixed app colors

--- app/src/main/java/com/example/huc_project/homepage/Homepage.java
+++ app/src/main/java/com/example/huc_project/homepage/Homepage.java
@@ -316,7 +316,7 @@ public class Homepage extends AppCompatActivity implements RecyclerViewAdapter.O
         ImageButton slidebutton = (ImageButton) sliding.getActionView();
         slidebutton.setBackgroundDrawable(null);
         slidebutton.setImageDrawable(myDrawable);
-        slidebutton.setColorFilter( Color.parseColor("#000000"));
+        //slidebutton.setColorFilter( Color.parseColor("#000000"));
         slidebutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // your handler code here
