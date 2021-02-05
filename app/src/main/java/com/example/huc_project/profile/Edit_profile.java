e28d85fd0043f527bd3c29c8d68414e20dc07b28
valenico <valerio.nicolanti@hotmail.it>
2021-02-05 09:45:40 +0100

    fix

--- app/src/main/java/com/example/huc_project/profile/Edit_profile.java
+++ app/src/main/java/com/example/huc_project/profile/Edit_profile.java
@@ -9,6 +9,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Criteria;
 import android.location.Geocoder;
@@ -35,7 +37,9 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.app.ActivityCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -119,6 +123,11 @@ public class Edit_profile extends AppCompatActivity {
 
 
                         final ImageButton getpositionButton = (ImageButton) findViewById(R.id.getPosition);
+                        Drawable like = AppCompatResources.getDrawable(getApplicationContext(), R.drawable.mylocation);
+
+                        final Drawable wrappedDrawable = DrawableCompat.wrap(like);
+                        DrawableCompat.setTint(wrappedDrawable, getResources().getColor(R.color.color_button1));
+                        getpositionButton.setImageDrawable(wrappedDrawable);
                         getpositionButton.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
@@ -288,6 +297,7 @@ public class Edit_profile extends AppCompatActivity {
                 e.printStackTrace();
             }
             TextView user_country = (TextView) findViewById(R.id.country);
+            
             user_country.setText(addresses.get(0).getAddressLine(0));
             Log.d("Location Changes", addresses.toString());
         }
